ab5e1a549a91cb0093a9b6bbd4526a4c3f9b8768
Sebastian Schenk <schenk.seb@gmail.com>
2022-07-02 21:28:33 +0100

    Add solution to exercise-02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(inputString, nTimes) {
+    let repeatedString = '';
 
+    if (nTimes < 0) {
+        return 'ERROR';
+    }
+
+    for (let i = 1; i <= nTimes; i++) {
+        repeatedString += inputString;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
