6c0470cedf3a7328841f04cb275ca0670bef34bf
Sebastian Schenk <schenk.seb@gmail.com>
2022-07-02 21:38:31 +0100

    Add solution to exercise-03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(inString) {
+    const chars = inString.split("");
+    const charsReverse = chars.reverse();
+    let outString = '';
 
+    for (const char of charsReverse) {
+        outString += char;
+    }
+    return outString;
 };
 
 // Do not edit below this line
