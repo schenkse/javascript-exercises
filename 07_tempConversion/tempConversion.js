50159c94387909e3a29b13b2d82705b3a966bf19
Sebastian Schenk <schenk.seb@gmail.com>
2022-07-03 17:01:16 +0100

    Add solution to exercise-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * 5/9;
+  return round(celsius, 1);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  const fahrenheit = celsius * 9/5 + 32;
+  return round(fahrenheit, 1);
 };
+
+const round = function(number, decimalPlaces) {
+  const factorOfTen = Math.pow(10, decimalPlaces);
+  return Math.round(number * factorOfTen) / factorOfTen;
+}
 
 // Do not edit below this line
 module.exports = {
