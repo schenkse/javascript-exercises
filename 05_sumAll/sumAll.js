cc7e8c0b2e64a2191ae00d5f0c691f92d4c16e55
Sebastian Schenk <schenk.seb@gmail.com>
2022-07-03 16:47:19 +0100

    Add solution to exercise-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return 'ERROR';
+    if (start < 0 || end < 0) return 'ERROR';
+    if (end < start) {
+        const endCopy = end;
+        end = start;
+        start = endCopy;
+    }
+    const numbers = range(start, end);
+    let sum = 0;
+    for (const number of numbers) {
+        sum += number;
+    }
+    return sum;
 };
+
+function range(start, end) {
+    if (start === end) return [start];
+    return [start, ...range(start + 1, end)];
+}
 
 // Do not edit below this line
 module.exports = sumAll;
