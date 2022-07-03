17802d7a6311bb0e3a3a30ab07c618a048691aea
Sebastian Schenk <schenk.seb@gmail.com>
2022-07-03 16:26:09 +0100

    Add solution to exercise-04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inArray, ...args) {
+    let outArray = inArray;
+    for (const arg of args) {
+        outArray = outArray.filter(element => element !== arg);
+    }
+    return outArray;
 };
 
 // Do not edit below this line
