fb4e4ddee783c88a9db5c1b81e2bc8365876b46e
Sebastian Schenk <schenk.seb@gmail.com>
2022-07-04 22:17:31 +0100

    Add solution to exercise-09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const stringClean = string.replace(/[,!. ]/g, '').toLowerCase();
+    const stringReverse = reverseString(stringClean);
+    return stringClean == stringReverse;
+};
 
+const reverseString = function(inString) {
+    return inString.split('').reverse().join('');
 };
 
 // Do not edit below this line
