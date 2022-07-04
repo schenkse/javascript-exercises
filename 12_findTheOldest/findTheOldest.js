ee7f97fe2c7bb962530f506f5a42e1eaf19e9a8a
Sebastian Schenk <schenk.seb@gmail.com>
2022-07-04 22:52:11 +0100

    Add solution to exercise-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const peopleByAge = people.sort(function(a, b) {
+        let agePrevPerson = a.yearOfBirth;
+        let ageNextPerson = b.yearOfBirth;
+        if (a.yearOfDeath === undefined) {
+            agePrevPerson -= new Date().getFullYear();
+        } else {
+            agePrevPerson -= a.yearOfDeath;
+        }
+        if (b.yearOfDeath === undefined) {
+            ageNextPerson -= new Date().getFullYear();
+        } else {
+            ageNextPerson -= b.yearOfDeath;
+        }
+        return ageNextPerson >= agePrevPerson ? -1 : 1;
+    });
+    return peopleByAge[0];
 };
 
 // Do not edit below this line
